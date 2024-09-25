fe74aeb62723d019401ec07a233504b4180122e0
mbsandur11 <mbsandur143@gmail.com>
2024-09-25 23:52:06 +0530

    app latest

--- static/tests/test_app.py
+++ static/tests/test_app.py
@@ -5,7 +5,6 @@ from app import app, db, Product  # Ensure 'app.py' is in the same directory or 
 def client():
     with app.test_client() as client:
         yield client
-
 def test_index(client):
     response = client.get('/')
     assert b'Welcome to the Simple Store!' in response.data
